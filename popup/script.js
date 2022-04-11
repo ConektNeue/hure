fb7828e9a628a06045020d5f50e1006a91c8ca91
went <97319785+ConektNeue@users.noreply.github.com>
2022-04-11 22:53:50 +0200

    json

--- popup/script.js
+++ popup/script.js
@@ -1,3 +1,33 @@
+let accountsURL = 'https://entpp.conekt.repl.co/accounts.json';
+let jsonData = null;
+
+function findProp(obj, prop, defval){
+    if (typeof defval == 'undefined') defval = null;
+    prop = prop.split('.');
+    for (var i = 0; i < prop.length; i++) {
+        if(typeof obj[prop[i]] == 'undefined')
+            return defval;
+        obj = obj[prop[i]];
+    }
+    return obj;
+}
+
+function sendRequest(url, accountName, field) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+        jsonData = request.response;
+        // return jsonData[accountName][data];
+        let props = 'eloandenefle.avatar';
+        let propstwo = 'eloandenefle.banner';
+        document.querySelector('.user-home>.avatar').style.backgroundImage = 'url(' + findProp(jsonData, props) + ')';
+        document.querySelector('.user-home>.banner').style.backgroundImage = 'url(' + findProp(jsonData, propstwo) + ')';
+    }
+}
+
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 
@@ -12,9 +42,9 @@ document.getElementById('btn-submit').addEventListener('click', function () {
 });
 
 function login() {
-    if (email.value === 'Desarollador' && password.value === 'bumbum@78!') {
-        localStorage.setItem('user', 'Desarollador');
-        localStorage.setItem('password', 'bumbum@78!');
+    if (email.value === 'a' && password.value === 'b') {
+        localStorage.setItem('user', 'a');
+        localStorage.setItem('password', 'b');
         initializeSession('developer');
     } else {
         alert('Email ou mot de passe incorrect.');
@@ -22,5 +52,10 @@ function login() {
 }
 
 function initializeSession(name) {
-    alert('bang!');
+    if (name === 'developer') {
+        document.querySelector('.login').style.display = 'none';
+        document.querySelector('.user-home').style.display = 'block';
+        // let ho = sendRequest(accountsURL, 'eloandenefle', 'name');
+        sendRequest(accountsURL, 'username');
+    }
 }
